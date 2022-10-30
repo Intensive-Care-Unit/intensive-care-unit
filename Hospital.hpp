388e9a24dd441bc555bc6c701addc5abe7569a30
salah <salah.mak@protonmail.com>
2022-10-31 00:01:51 +0100

    add some comments for future work

--- Hospital.hpp
+++ Hospital.hpp
@@ -6,10 +6,18 @@
 class Hospital
 {
 public:
+    // initialize Hospital with at least Director
 
 private:
     std::vector<CareUnit> _services;
 
     bool addUnit(const std::string &unit);
 
+    // Director
+
+    // array of staff
+
 };
+
+
+// TODO: make Staff classes
