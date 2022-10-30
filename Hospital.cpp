31db45330735c3b8fcefd15f5b8f2dd98570480f
salah <salah.mak@protonmail.com>
2022-10-30 23:35:19 +0100

    Initial implementation of some interfaces (CareUnit, Hospital, Measurement, Patient)

---
+++ Hospital.cpp
@@ -0,0 +1,26 @@
+
+#include "Hospital.hpp"
+#include <stdexcept>
+#include <algorithm>
+
+bool Hospital::addUnit(const std::string &name)
+{
+    // black magic :)
+    // checking if unit of name "name" exists in the list of services
+    // here, used lambda function to check with the object's attribute instead of the entire object
+    if (std::find_if(_services.begin(), _services.end(), [name](const CareUnit &unit)
+    { return unit.getServiceName() != name; }) == _services.end())
+    {
+        // if it doesn't, we add a new service and return true
+
+        // https://yasenh.github.io/post/cpp-diary-1-emplace_back/
+        _services.emplace_back(name);
+        return true;
+    } else
+    {
+        // otherwise, if it exists, we return false
+        return false;
+    }
+
+
+}
