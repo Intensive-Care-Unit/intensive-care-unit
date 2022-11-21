759f5f43018a79c4d7b1f084885f990b6c56ed0d
Iyad <shinobisan38@gmail.com>
2022-11-21 22:15:02 +0000

    new measure every 2 secs instead of 3

--- Hashmap/Test.cpp
+++ Hashmap/Test.cpp
@@ -1,44 +1,44 @@
-/** Mohammed Iyad Benkhaled **/
-#include "State.hpp"
-#include <iostream>
-#include <fstream>
-#include <chrono>
+// /** Mohammed Iyad Benkhaled **/
+// #include "State.hpp"
+// #include <iostream>
+// #include <fstream>
+// #include <chrono>
 
-using namespace std;
-using namespace chrono;
+// using namespace std;
+// using namespace chrono;
 
 
-int main()
-{
-    auto unit = State::getHospital()->getCriticalUnit();
+// int main()
+// {
+//     auto unit = State::getHospital()->getCriticalUnit();
 
-    ofstream csv;
-    csv.open("addPatients-HM.csv");
+//     ofstream csv;
+//     csv.open("addPatients-HM.csv");
 
-    csv << "n,t" << endl;
+//     csv << "n,t" << endl;
 
-    int EXP = 20;
+//     int EXP = 20;
 
-    for (int i = 1; i <= 100000; i++)
-    {
-        long long int average = 0;
-        for (int a = 1; a <= EXP; a++)
-        {
-            string istr = to_string(i);
-            auto start = steady_clock::now();
-            unit.addPatient({"critical", "name " + istr, 'm', 19, 150, 90});
-            auto end = steady_clock::now();
+//     for (int i = 1; i <= 100000; i++)
+//     {
+//         long long int average = 0;
+//         for (int a = 1; a <= EXP; a++)
+//         {
+//             string istr = to_string(i);
+//             auto start = steady_clock::now();
+//             unit.addPatient({"critical", "name " + istr, 'm', 19, 150, 90});
+//             auto end = steady_clock::now();
 
-            auto duration = (long long int) chrono::duration_cast<chrono::nanoseconds>(end - start).count();
+//             auto duration = (long long int) chrono::duration_cast<chrono::nanoseconds>(end - start).count();
 
-            average += duration / EXP;
+//             average += duration / EXP;
 
-        }
+//         }
 
-        if (i % 100 == 0)
-        {
-            csv << to_string(unit.getPatients().size()) << "," << to_string(average) << endl;
-        }
-    }
+//         if (i % 100 == 0)
+//         {
+//             csv << to_string(unit.getPatients().size()) << "," << to_string(average) << endl;
+//         }
+//     }
 
-}
+// }
