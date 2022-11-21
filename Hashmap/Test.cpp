b7138add5beca97fa573fe923e32767322aa9c99
salah <salah.mak@protonmail.com>
2022-11-21 23:35:58 +0100

    final commit

--- Hashmap/Test.cpp
+++ Hashmap/Test.cpp
@@ -1,44 +1,36 @@
-// /** Mohammed Iyad Benkhaled **/
-// #include "State.hpp"
-// #include <iostream>
-// #include <fstream>
-// #include <chrono>
 
-// using namespace std;
-// using namespace chrono;
+#include "State.hpp"
+#include <iostream>
+#include <fstream>
+#include <chrono>
+
+using namespace std;
+using namespace chrono;
 
 
-// int main()
-// {
-//     auto unit = State::getHospital()->getCriticalUnit();
+int main()
+{
+    auto unit = State::getHospital()->getCriticalUnit();
 
-//     ofstream csv;
-//     csv.open("addPatients-HM.csv");
+    ofstream csv;
+    csv.open("addPatients-HM.csv");
 
-//     csv << "n,t" << endl;
+    csv << "n,t" << endl;
 
-//     int EXP = 20;
 
-//     for (int i = 1; i <= 100000; i++)
-//     {
-//         long long int average = 0;
-//         for (int a = 1; a <= EXP; a++)
-//         {
-//             string istr = to_string(i);
-//             auto start = steady_clock::now();
-//             unit.addPatient({"critical", "name " + istr, 'm', 19, 150, 90});
-//             auto end = steady_clock::now();
+    for (int i = 1; i <= 100000; i++)
+    {
+        string istr = to_string(i);
+        auto start = steady_clock::now();
+        unit.addPatient({"critical", "name " + istr, 'm', 19, 150, 90});
+        auto end = steady_clock::now();
 
-//             auto duration = (long long int) chrono::duration_cast<chrono::nanoseconds>(end - start).count();
+        if (i % 100 == 0)
+        {
+            auto duration = (long long int) chrono::duration_cast<chrono::nanoseconds>(end - start).count();
 
-//             average += duration / EXP;
+            csv << to_string(unit.getPatients().size()) << "," << to_string(duration) << endl;
+        }
+    }
 
-//         }
-
-//         if (i % 100 == 0)
-//         {
-//             csv << to_string(unit.getPatients().size()) << "," << to_string(average) << endl;
-//         }
-//     }
-
-// }
+}
